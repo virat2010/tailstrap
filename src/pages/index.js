916d00af95de3c637c88797ff778b7b133b00a9f
Virat <59180983+virat2010@users.noreply.github.com>
2023-01-18 22:13:01 -0500

    Add opacity, and shadow. Clean up stuff. Bunch more CSS stuff as well. Still no real contribution. Doing a lot of research though.

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,9 @@ function index() {
       <Head><title>Get started with Tailstrap</title></Head>
       <H1>Get started with Tailstrap</H1>
       <Subheading>Tailstrap is a bootstrap to tailwind toolkit. Build anything-from bootstrap to tailwind-in seconds.</Subheading>
+      
+      <p>This is a long gone project and just a side-project started by Christian Genco and Virat R. Ponugoti. If you really want to take this seriously then either make a PR at the github page or if you are just a user go check out <a href="https://mdbootstrap.com/">MDBootstrap</a> or <a href="https://flowbite.com">Flowbite</a>. <br></br>They got serious with bootstrap and they also built it in tailwind. This project has versatality as there are no pre-built components. By the way, thank you for checking out this project.</p>    
+    
     </div>)
 }
 export default index
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
   },
   plugins: [],
 }
